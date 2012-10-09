2a326e291ff63e6edd680edaabf258375a8ace7c
vimil <vimilsaju@yahoo.com>
2012-10-09 13:40:53 -0700

    anges to use javadb

---
+++ src/main/java/com/cwctravel/hudson/plugins/suitegroupedtests/junit/io/IOUtil.java
@@ -0,0 +1,29 @@
+package com.cwctravel.hudson.plugins.suitegroupedtests.junit.io;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+
+public class IOUtil {
+	public static ReaderWriter createReaderWriter(char[] data, int offset, int length) throws IOException {
+		ReaderWriter result = null;
+		if(length < 1024 * 1024) {
+			result = new StringReaderWriter(data, offset, length);
+		}
+		else {
+			result = new TempFileReaderWriter(data, offset, length);
+		}
+		return result;
+	}
+
+	public static void write(Reader reader, Writer writer) throws IOException {
+		if(reader != null && writer != null) {
+			char[] buffer = new char[1024];
+			int bytesRead = 0;
+			while((bytesRead = reader.read(buffer)) >= 0) {
+				writer.write(buffer, 0, bytesRead);
+			}
+			writer.flush();
+		}
+	}
+}
